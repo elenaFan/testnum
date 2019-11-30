2a71c40d50f2452d3f0a3d98118f37e8850ab6bc
elenaFan <415109102@qq.com>
2019-11-30 13:48:48 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App" :realNum="num" :clientWidth="clientWidth"/>
+    <!-- <countTo :startVal='startVal' :endVal='endVal' :duration='3000'></countTo> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import countTo from 'vue-count-to';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    countTo
+  },
+  data(){
+    return{
+      startVal: 0,
+        endVal: 201798765,
+        num:[9,8,7,1],
+        clientWidth:1990
+    }
+  },
+  created() {
+    this.clientWidth = document.body.clientWidth
   }
 }
 </script>
---
+++ src/components/test.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="allNumCon" class="allNumCon">
+    <div v-for="item in list" :key="'number' + item">
+      <div class="OneNumCon">
+        <div :class="'rolling_' + dataNumStr[item - 1]" :style="{'transform': 'translate3d(0,-' + 30 * numStr[item - 1] + 'px, 0)'}" class="OneListNum">
+          <!-- <div><img :src="require('../../assets/image/index/0.png')" alt=""></div> -->
+          <div>0</div>
+          <div v-for="num in [1,2,3,4,5,6,7,8,9,0]" :key="num">
+            num
+            <!-- <img :src="require('../../assets/image/index/'+(num)+'.png')" alt=""> -->
+            </div>
+        </div>
+      </div><div v-if="(list - item) % 3 === 0 && (list - item) !== 0" class="OneNumCon_dou">
+        <!-- <img :src="require('../../assets/image/index/point.png')" alt=""> -->
+        ,
+        </div>
+    </div>
+  </div>
+  <div v-else id="allNumCon" class="allNumCon">
+    <!-- <img :src="require('../../assets/image/index/0.png')" alt=""> -->
+    ,
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
